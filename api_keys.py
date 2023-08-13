b888e997cd75680ffaffbd5ec4d42d73f81dd861
kalikschoenrock <kalikschoenrock@yahoo.com>
2023-08-12 20:40:17 -0500

    API Challenge

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "24425bc7c9000c3b7e04a7e1c3e35f41"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "85971f193d0b4fa5853c0a16ae90de8a"
